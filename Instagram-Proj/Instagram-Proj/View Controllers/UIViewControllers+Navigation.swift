3e4691e3ef685f2058698e8ee0e6de62b861bf20
maitreebain <maitreebain@gmail.com>
2020-03-03 17:09:58 -0500

    auth sash added, working on editing

---
+++ Instagram-Proj/Instagram-Proj/View Controllers/UIViewControllers+Navigation.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewControllers+Navigation.swift
+//  Instagram-Proj
+//
+//  Created by Maitree Bain on 3/3/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    private static func resetWindow(_ rootViewController: UIViewController) {
+        guard let scene = UIApplication.shared.connectedScenes.first,
+            let sceneDelegate = scene.delegate as? SceneDelegate,
+            let window = sceneDelegate.window else {
+                fatalError("could not reset window rootViewController")
+        }
+        window.rootViewController = rootViewController
+    }
+    
+    public static func showViewController(storyboardName: String, viewControllerID: String) {
+        
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let newVC = storyboard.instantiateViewController(withIdentifier: viewControllerID)
+        resetWindow(newVC)
+    }
+
+    
+}
