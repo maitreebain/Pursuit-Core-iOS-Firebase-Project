042cbe4a23f130e231286a532f13791c1c6916b7
maitreebain <maitreebain@gmail.com>
2020-03-03 15:28:12 -0500

    added vcs

---
+++ Instagram-Proj/Instagram-Proj/View Controllers/ImagesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImagesViewController.swift
+//  Instagram-Proj
+//
+//  Created by Maitree Bain on 3/3/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ImagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram-Proj/Instagram-Proj/View Controllers/PostsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PostsViewController.swift
+//  Instagram-Proj
+//
+//  Created by Maitree Bain on 3/3/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+}
