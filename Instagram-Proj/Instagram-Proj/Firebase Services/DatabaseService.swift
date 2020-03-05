354f3b4e333bdc67d8becf8a3aba35f7d2d4cae3
maitreebain <maitreebain@gmail.com>
2020-03-05 16:58:53 -0500

    working on createPost for database

--- Instagram-Proj/Instagram-Proj/Firebase Services/DatabaseService.swift
+++ Instagram-Proj/Instagram-Proj/Firebase Services/DatabaseService.swift
@@ -1,5 +1,5 @@
 //
-//  DatabaseStorageSession.swift
+//  DatabaseService.swift
 //  Instagram-Proj
 //
 //  Created by Maitree Bain on 3/5/20.
@@ -7,3 +7,16 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class DatabaseService {
+    
+    static let userPost = "userPost"
+    
+    private let db = Firestore.firestore()
+    
+    public func createUserPost(imageURL: String, description: String, displayName: String, completion: @escaping (Result<String, Error>) -> ()) {
+        
+    }
+}
